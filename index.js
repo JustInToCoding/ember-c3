99fd43002ff0f81245d1c0df255768d67d529f67
Justin Steenhuis <justinsteenhuis@gmail.com>
2017-01-18 14:18:58 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ module.exports = {
   name: 'ember-c3',
   included: function(app) {
     this._super.included(app);
-
-    app.import(app.bowerDirectory + '/d3/d3.min.js');
-    app.import(app.bowerDirectory + '/c3/c3.min.css');
-    app.import(app.bowerDirectory + '/c3/c3.min.js');
+    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
+      app.import(app.bowerDirectory + '/d3/d3.min.js');
+      app.import(app.bowerDirectory + '/c3/c3.min.js');
+      app.import(app.bowerDirectory + '/c3/c3.min.css');
+      app.import('vendor/ember-c3.js', {
+        exports: {
+          c3: ['default']
+        }
+      });
+    }
   }
 };
